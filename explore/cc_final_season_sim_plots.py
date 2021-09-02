e3c173586f7f621002cfd4358583eb466aabfb5e
Ryan Davis <ryandavis@ryans-mbp-2.attlocal.net>
2021-09-02 12:15:10 -0400

    update gitignore

---
+++ explore/cc_final_season_sim_plots.py
@@ -0,0 +1,104 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from utils.constants import Teams
+
+# Pandas options for better printing
+
+pd.set_option('display.max_columns', 500)
+pd.set_option('display.max_rows', 1000)
+pd.set_option('display.width', 1000)
+
+final_tables_west = pd.read_csv('results/sim_results_west.csv')
+final_tables_east = pd.read_csv('results/sim_results_east.csv')
+
+
+def label_bars(rects, num_simulations):
+    for rect in rects:
+        height = rect.get_width()
+        y = round(height * num_simulations)
+        if y > 0:
+            plt.text(height + 0.005, rect.get_y() + rect.get_height() / 2, y, fontweight='bold', ha='left', va='center')
+
+
+def calc_ticks(hist):
+    min_val = min(hist.index)
+    max_val = max(hist.index)
+    return list(range(min_val, max_val + 1))
+
+
+def plot_team(frame, team, division_size):
+    print(team)
+    sims = frame.shape[0]
+    point_hist = frame['league_points'].value_counts().sort_index()
+    wins_hist = frame['wins'].value_counts().sort_index()
+    rank_hist = frame['rank'].value_counts().sort_index()
+
+    color = Teams.TeamColors[team]
+
+    fig = plt.figure(figsize=(12, 9))
+    fig.suptitle('{} Simulated Season Results ({} Simulations)'.format(team, sims), fontsize=20, y=.95)
+
+    plt.subplot(3, 1, 1)
+    rects = plt.barh(rank_hist.index, rank_hist.values / sims, color=color)
+    label_bars(rects, sims)
+    plt.ylabel('Final Standing')
+    plt.xlim((0, 1))
+    ticks = list(range(1, division_size+1))
+    plt.yticks(ticks)
+    plt.gca().invert_yaxis()
+
+
+    plt.subplot(3, 1, 2)
+    rects = plt.barh(point_hist.index, point_hist.values / sims, color=color)
+    label_bars(rects, sims)
+    plt.ylabel('League Points')
+    plt.xlim((0, 1))
+    ticks = calc_ticks(point_hist)
+    plt.yticks(ticks)
+
+
+    plt.subplot(3, 1, 3)
+    rects = plt.barh(wins_hist.index, wins_hist.values / sims, color=color)
+    label_bars(rects, sims)
+    plt.ylabel('Wins')
+    plt.xlim((0, 1))
+    plt.xlabel('Frequency')
+
+    ticks = calc_ticks(wins_hist)
+    plt.yticks(ticks)
+
+    plt.savefig('standing_plots/{}'.format(team))
+
+    return 100 * rank_hist / sims
+
+
+west_ranks = []
+east_ranks = []
+
+for team in Teams.West:
+    team_results = final_tables_west[final_tables_west['team'] == team][['league_points', 'wins', 'rank']]
+    ranks = plot_team(team_results, team, len(Teams.West))
+    ranks['team'] = team
+    west_ranks.append(ranks)
+
+for team in Teams.East:
+    team_results = final_tables_east[final_tables_east['team'] == team][['league_points', 'wins', 'rank']]
+    ranks = plot_team(team_results, team, len(Teams.East))
+    ranks['team'] = team
+    east_ranks.append(ranks)
+
+west_ranks = pd.DataFrame(west_ranks)
+east_ranks = pd.DataFrame(east_ranks)
+
+west_ranks = west_ranks.fillna(0.0)
+west_columns = ['team', 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+west_ranks = west_ranks[west_columns]
+west_ranks = west_ranks.sort_values(by=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12], ascending=False)
+print(west_ranks)
+
+east_ranks = east_ranks.fillna(0.0)
+east_columns = ['team', 1, 2, 3, 4, 5, 6, 7, 8]
+east_ranks = east_ranks[east_columns]
+east_ranks = east_ranks.sort_values(by=[1, 2, 3, 4, 5, 6, 7, 8], ascending=False)
+
+print(east_ranks)
---
+++ explore/team_rating_by_week.py
@@ -0,0 +1,186 @@
+import pandas as pd
+import numpy as np
+from sklearn.linear_model import RidgeCV
+from sklearn import metrics
+
+from utils.constants import Maps
+
+# Pandas options for better printing
+pd.set_option('display.max_columns', 500)
+pd.set_option('display.max_rows', 1000)
+pd.set_option('display.width', 1000)
+
+# Read in our scored maps generated in the map_score script
+map_scores = pd.read_csv('results/scored_maps.csv')
+
+# Limit our results to 2021 maps
+map_scores = map_scores[map_scores['season'] == 2021]
+
+# Duplicate and mirror our map scores so that we can get a row for each team on attack and defense on each map
+map_scores_swapped = map_scores.copy(deep=True)
+map_scores_swapped['team_one_score'] = map_scores['team_two_score']
+map_scores_swapped['team_two_score'] = map_scores['team_one_score']
+map_scores_swapped['team_one_name'] = map_scores['team_two_name']
+map_scores_swapped['team_two_name'] = map_scores['team_one_name']
+map_scores = pd.concat([map_scores_swapped, map_scores])
+map_scores = map_scores.dropna()
+
+# Generate a list of teams
+teams = list(set(list(map_scores['team_one_name'].values) + list(map_scores['team_two_name'].values)))
+teams = [str(p) for p in teams]
+teams = sorted(teams)
+
+
+# Convert an input row into a row of our sparse matrix
+def map_teams(row_in, teams):
+    t1 = row_in[0]
+    t2 = row_in[1]
+
+    row_out = np.zeros([len(teams) * 2])
+
+    row_out[teams.index(t1)] = 1
+    row_out[teams.index(t2) + len(teams)] = -1
+    return row_out
+
+
+# Take in our input data, convert the teams into our sparse design matrix and the map scores into our target column
+def extract_X_Y(frame):
+    stints_x_base = frame[['team_one_name', 'team_two_name']].values
+
+    stint_X_rows = np.apply_along_axis(map_teams, 1, stints_x_base, teams)
+
+    stint_Y_rows = frame[['team_one_score']].values
+    return stint_X_rows, stint_Y_rows
+
+
+# Convert lambda value to alpha needed for ridge CV
+def lambda_to_alpha(lambda_value, samples):
+    return (lambda_value * samples) / 2.0
+
+
+# Convert RidgeCV alpha back into a lambda value
+def alpha_to_lambda(alpha_value, samples):
+    return (alpha_value * 2.0) / samples
+
+
+# Calculate Regularized Map Type Score
+def calculate_rmts(stint_X_rows, stint_Y_rows, map_type):
+    # We will perform cross validation across a number of different lambdas
+    lambdas = [.01, 0.025, .05, 0.075, .1, .125, .15, .175, .2, .225, .25]
+
+    # convert the lambdas into alpha values
+    alphas = [lambda_to_alpha(l, stint_X_rows.shape[0]) for l in lambdas]
+
+    # Create our ridge CV model
+    clf = RidgeCV(alphas=alphas, cv=5, fit_intercept=True, normalize=False)
+
+    # Fit our data
+    model = clf.fit(stint_X_rows, stint_Y_rows)
+
+    # extract our teams, and coefficients and combine them into a single matrix (20 x 3)
+    team_arr = np.transpose(np.array(teams).reshape(1, len(teams)))
+    coef_array_attack = np.transpose(model.coef_[:, 0:len(teams)])
+    coef_array_def = np.transpose(model.coef_[:, len(teams):])
+    team_coef_arr = np.concatenate([team_arr, coef_array_attack, coef_array_def], axis=1)
+
+    # build a dataframe from our matrix
+    rmts = pd.DataFrame(team_coef_arr)
+    intercept = model.intercept_[0]
+
+    # Rename columns to include the current map type
+    attack_str = 'rmsa attack'
+    defend_str = 'rmsa defend'
+
+    rmts.columns = ['team', attack_str, defend_str]
+    rmts[attack_str] = rmts[attack_str].astype(float)
+    rmts[defend_str] = rmts[defend_str].astype(float)
+
+    # Calculate a total RMSA
+    rmts['rmsa'] = rmts[attack_str] + rmts[defend_str]
+    rmts['intercept'] = intercept
+    rmts['map_type'] = map_type
+
+    # Generate a couple of error statistics
+    lambda_picked = alpha_to_lambda(model.alpha_, stint_X_rows.shape[0])
+    print('r^2: ', model.score(stint_X_rows, stint_Y_rows))
+    print('lambda: ', lambda_picked)
+    print('intercept: ', intercept)
+
+    pred = model.predict(stint_X_rows)
+    print('MAE: ', metrics.mean_absolute_error(stint_Y_rows, pred))
+    print('MSE: ', metrics.mean_squared_error(stint_Y_rows, pred))
+    rmts = rmts.sort_values(by='rmsa', ascending=False)
+
+    ## Closed Form
+    # B = (XtX + lambdaI)^-1 * XtY
+    intercept_row = np.ones((stint_X_rows.shape[0], 1))
+    close_form_stint_x = np.concatenate([stint_X_rows, intercept_row], axis=1)
+    xtx = np.matmul(np.transpose(close_form_stint_x), close_form_stint_x) + lambda_to_alpha(lambda_picked,stint_X_rows.shape[0])  * np.eye(
+        close_form_stint_x.shape[1])
+    xtx_inv = np.linalg.inv(xtx)
+    xty = np.matmul(np.transpose(close_form_stint_x), stint_Y_rows)
+    betas = np.matmul(xtx_inv, xty)
+
+    pred = np.matmul(close_form_stint_x, betas)
+    resid = stint_Y_rows - pred
+
+
+    scalar = 1 / (close_form_stint_x.shape[0] - 40 - 1)
+
+    errs = np.matmul(np.transpose(resid), resid)
+    var = errs * xtx_inv * scalar
+
+    var_map = {}
+    for ind, team in enumerate(team_arr):
+
+        attack_var = var[ind][ind]
+        defend_var = var[ind + len(team_arr)][ind + len(team_arr)]
+        var_map[team[0]] = {'team': team[0], 'attack_variance': attack_var,
+                         'defend_variance': defend_var}
+    variance = pd.DataFrame(var_map.values())
+
+    rmts = rmts.merge(variance, on='team')
+    rmts['rmsa attack stdev'] = np.sqrt(rmts['attack_variance'])
+    rmts['rmsa defend stdev'] = np.sqrt(rmts['defend_variance'])
+    rmts = rmts[['team', 'rmsa attack', 'rmsa attack stdev', 'rmsa defend', 'rmsa defend stdev', 'rmsa', 'map_type']]
+    rmts['normalized rmsa'] = 100 * ((2 * (rmts['rmsa'] - rmts['rmsa'].min())/(rmts['rmsa'].max()-rmts['rmsa'].min())) - 1)
+    print(rmts)
+    return rmts
+
+# Generate RMSA for control maps
+print('control')
+control = map_scores[map_scores['map_type'] == Maps.Control]
+control_X, control_Y = extract_X_Y(control)
+control_rmts = calculate_rmts(control_X, control_Y, Maps.Control)
+
+# Generate RMSA for Escort maps
+print('escort')
+escort = map_scores[map_scores['map_type'] == Maps.Escort]
+escort_X, escort_Y = extract_X_Y(escort)
+escort_rmts = calculate_rmts(escort_X, escort_Y, Maps.Escort)
+
+# Generate RMSA for Hybrid maps
+print('hybrid')
+hybrid = map_scores[map_scores['map_type'] == Maps.Hybrid]
+hybrid_X, hybrid_Y = extract_X_Y(hybrid)
+hybrid_rmts = calculate_rmts(hybrid_X, hybrid_Y, Maps.Hybrid)
+
+# Generate RMSA for Assault maps
+print('assault')
+assault = map_scores[map_scores['map_type'] == Maps.Assault]
+assault_X, assault_Y = extract_X_Y(assault)
+assault_rmts = calculate_rmts(assault_X, assault_Y, Maps.Assault)
+
+all_rmsa = pd.concat([control_rmts, escort_rmts, hybrid_rmts, assault_rmts])
+
+all_rmsa.to_csv('results/rmsa.csv', index=False)
+
+
+def calculate_total_rmsa(group):
+    total = group['normalized rmsa'].sum() + group[group['map_type'] == Maps.Control]['normalized rmsa'].sum()
+    return pd.Series({'rmsa': total/5})
+
+
+total_rmsa = all_rmsa[['team', 'map_type', 'normalized rmsa']].groupby(by='team').apply(
+    calculate_total_rmsa).reset_index().sort_values(by='rmsa', ascending=False)
+print(round(total_rmsa, 2))
